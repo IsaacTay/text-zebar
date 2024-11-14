57ab5bd7090dbdeee6540146a192a704f86559f8
Isaac Tay <IsaacTay@pm.me>
2024-11-14 09:08:30 +0000

    fix: removed ability to highlight button text

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,11 +6,10 @@
 		class: string;
 	};
 	let { iconClass, label, callback, ...rest }: ButtonProps = $props();
-	//class="flex bg-blend-30 border border-zb-border rounded-base shadow-button hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none p-1 {rest.class}"
 </script>
 
 <button
-	class="flex bg-zb-ws-base text-zb-ws-base-text hover:bg-zb-ws-hover hover:text-zb-ws-hover-text {rest.class}"
+	class="flex bg-zb-ws-base text-zb-ws-base-text hover:bg-zb-ws-hover hover:text-zb-ws-hover-text select-none {rest.class}"
 	onclick={callback}
 	aria-label={label}
 >
