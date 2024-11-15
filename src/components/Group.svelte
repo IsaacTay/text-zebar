65423a5832b7a6ca4859c4b8c5f6ca0f4e10165d
Isaac Tay <IsaacTay@pm.me>
2024-11-15 09:31:16 +0000

    feat: add transition animation for border hover

--- src/components/Group.svelte
+++ src/components/Group.svelte
@@ -9,7 +9,7 @@
 
 <div class="h-full w-fit bg-zb-base p-2 pt-3 {rest.class}">
 	<div
-		class="h-full w-fit rounded-base border border-zb-border bg-zb-base text-zb-border hover:border-zb-border_focused hover:text-zb-border_focused {rest.class}"
+		class="h-full w-fit rounded-base border border-zb-border bg-zb-base text-zb-border hover:border-zb-border_focused hover:text-zb-border_focused transition-all {rest.class}"
 	>
 		<div class="absolute z-10 mx-2 -mt-3.5 bg-zb-base px-2">{group}</div>
 		<div class="flex h-full w-fit items-center rounded-base bg-zb-base p-2.5 text-zb-text">
