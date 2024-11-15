3f66963866515c012788e40fc65db640001e1b49
Isaac Tay <IsaacTay@pm.me>
2024-11-15 02:59:01 +0000

    feat: changed all icons to nerdfonts

--- src/components/RightGroup.svelte
+++ src/components/RightGroup.svelte
@@ -18,55 +18,40 @@
 
 	let { date, glazewm, network, weather, battery }: RightGroupProps = $props();
 	let battery_symbols = '󰂎󰁺󰁻󰁼󰁽󰁾󰁿󰂀󰂁󰂂󰁹';
+	let wifi_symbols = '󰤯󰤟󰤢󰤥󰤨';
+	let weather_symbols = {
+		clear_day: '',
+		clear_night: '',
+		cloudy_day: '',
+		cloudy_night: '',
+		light_rain_day: '',
+		light_rain_night: '',
+		heavy_rain_day: '',
+		heavy_rain_night: '',
+		snow_day: '',
+		snow_night: '',
+		thunder_day: '',
+		thunder_night: ''
+	};
+  let clock_symbols = "󱑖󱑋󱑌󱑍󱑎󱑏󱑐󱑑󱑒󱑓󱑔󱑕"
 	//<NowPlaying {glazewm} />
 </script>
 
 <div class="flex flex-row items-center gap-3">
 	<div class="flex flex-row items-center gap-1">
 		{#if network?.defaultInterface?.type === 'ethernet'}
-			<i class="nf nf-md-ethernet_cable"></i>
+			󰈀
 		{:else if network?.defaultInterface!.type === 'wifi'}
-			{#if network.defaultGateway!.signalStrength! >= 75}
-				<i class="nf nf-md-wifi_strength_4"></i>
-			{:else if network.defaultGateway!.signalStrength! >= 50}
-				<i class="nf nf-md-wifi_strength_2"></i>
-			{:else if network.defaultGateway!.signalStrength! >= 25}
-				<i class="nf nf-md-wifi_strength_1"></i>
-			{:else}
-				<i class="nf nf-md-wifi_strength_outline"></i>
-			{/if}
+			{@const index = Math.round(network.defaultGateway!.signalStrength! / 25) * 2}
+			{wifi_symbols.slice(index, index + 2)}
 			{network.defaultGateway?.ssid}
 		{:else}
-			<i class="nf nf-md-wifi_strength_off_outline"></i>
+			󰤮
 		{/if}
 	</div>
 	{#if weather}
 		<div>
-			{#if weather.status === 'clear_day'}
-				<i class="nf nf-weather-day_sunny"></i>
-			{:else if weather.status === 'clear_night'}
-				<i class="nf nf-weather-night_clear"></i>
-			{:else if weather.status === 'cloudy_day'}
-				<i class="nf nf-weather-day_cloudy"></i>
-			{:else if weather.status === 'cloudy_night'}
-				<i class="nf nf-weather-night_alt_cloudy"></i>
-			{:else if weather.status === 'light_rain_day'}
-				<i class="nf nf-weather-day_sprinkle"></i>
-			{:else if weather.status === 'light_rain_night'}
-				<i class="nf nf-weather-night_alt_sprinkle"></i>
-			{:else if weather.status === 'heavy_rain_day'}
-				<i class="nf nf-weather-day_rain"></i>
-			{:else if weather.status === 'heavy_rain_night'}
-				<i class="nf nf-weather-night_alt_rain"></i>
-			{:else if weather.status === 'snow_day'}
-				<i class="nf nf-weather-day_snow"></i>
-			{:else if weather.status === 'snow_night'}
-				<i class="nf nf-weather-night_alt_snow"></i>
-			{:else if weather.status === 'thunder_day'}
-				<i class="nf nf-weather-day_lightning"></i>
-			{:else if weather.status === 'thunder_night'}
-				<i class="nf nf-weather-night_alt_lightning"></i>
-			{/if}
+			{weather_symbols[weather.status]}
 			{Math.round(weather.celsiusTemp)}°
 		</div>
 	{/if}
@@ -81,7 +66,8 @@
 		</div>
 	{/if}
 	{#if date}
-		<i class="nf nf-md-clock"></i>
+    {@const index = +(date.iso.match(/T\d\d:/)[0].slice(1, 3)) * 2}
+    {clock_symbols.slice(index, index+2)}
 		{date?.formatted}
 	{/if}
 </div>
